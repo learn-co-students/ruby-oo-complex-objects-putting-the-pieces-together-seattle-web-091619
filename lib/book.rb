4e3a4b1ea550f392a4aecfbf0428e1cbfb5389d2
Packmanager9 <ColeJDitzler@gmail.com>
2019-09-18 14:52:14 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,49 @@
+class Book
+attr_accessor :author
 
+attr_accessor :page_count
+
+attr_accessor :genre
+
+
+
+    def initialize (titlex = "And Then There Were None", authorx = "Agatha Christie")
+
+        @title = titlex
+
+        @author = authorx
+    end
+
+
+    def title (titlex = "And Then There Were None")
+
+        @title = titlex
+    end
+
+
+
+
+    def author (authorx = "Agatha Christie")
+
+        @author = authorx
+    end
+
+
+    def page_count (page_countx = 272)
+        @page_count = page_countx
+    end
+
+
+    def genre (genrex = "Mystery")
+        @genre = genrex
+    end
+
+    def turn_page (pagex = 272)
+            @page = pagex+1
+             puts "Flipping the page...wow, you read fast!"
+            @page
+    end
+
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,45 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    
+    def initialize (brandx = "Nike", colorx = "red",  sizex = 9.5, materialx = "red", conditionx ="f")
+
+        @brand = brandx
+
+        @color = colorx
+
+        @size = sizex
+
+        @material = materialx
+
+        @condition = conditionx
+    end
+
+
+    def brand (brandx = "Nike")
+        @brand = brandx
+    end
+    
+    def color (brandx = "red")
+        @color = brandx
+    end
+    
+    def size (brandx = 9.5)
+        @size = brandx
+    end
+    
+    def material (brandx = "suede" )
+        @material = brandx
+    end
+    
+    def cobble
+            @condition = "new"
+            puts "Your shoe is as good as new!"
+    end
+
+end
