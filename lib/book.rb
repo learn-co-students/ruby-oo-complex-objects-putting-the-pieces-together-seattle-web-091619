aa52e1584f49b821d87f6b5118f9d1aa5ae1f3b9
Garrett Burns <GBmixo>
2019-09-19 10:32:19 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book
+    def initialize(title)
+        @title = title
+    end
 
+    #title
+    def title
+        @title
+    end
+
+    #author
+    def author=(author)
+        @author = author
+    end
+    def author
+        @author
+    end
+
+    #pages
+    def page_count=(num)
+        @page_count = num
+    end
+    def page_count
+        @page_count
+    end
+
+    #genre
+    def genre=(genre)
+        @genre = genre
+    end
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        
+    end
+end
 
